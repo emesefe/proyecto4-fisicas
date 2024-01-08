79ca27ff02a45572489b0488eeafda8bec4144f3
emesefe <maria@frogames.es>
2024-01-08 08:37:26 +0100

    Focal Point Script

---
+++ Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    [SerializeField] private float rotationSpeed = 10f;
+    private float horizontalInput;
+
+    private void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime * horizontalInput);
+    }
+}
